275b44ab922c157ac4a9d8bfd4715d722fff437b
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-07-15 09:31:47 +0300

    admin category inner page

--- src/routes/adminApiRouter.js
+++ src/routes/adminApiRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { postCategories } from '../api/admin/postCategories.js';
+import { deleteCategories } from '../api/admin/deleteCategories.js';
 
 export const adminApiRouter = express.Router();
 
 adminApiRouter.post('/categories', postCategories);
+adminApiRouter.delete('/categories/:url', deleteCategories);
